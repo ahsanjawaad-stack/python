e7622448e7985eb1d488c3ea5c50a8078fd855f7
ahsanjawaad-stack <ahsanjawaad@gmail.com>
2026-05-07 15:22:27 +0500

    sum using input

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,25 @@
-#write a program that sum two numbers.
-num1 = 10
-num2 = 20
-summ = num1 + num2
-print("the sum is :",summ)
+# #write a program that sum two numbers.
+# num1 = 10
+# num2 = 20
+# summ = num1 + num2
+# print("the sum is :",summ)
 
-#commit again
+# #commit again
+
+
+
+# user input
+
+var1 = int(input("enter first number"))
+var2 = int(input("enter second number"))
+# print(type(var1),type(var2),var1,var2)
+sum = var1 + var2
+print("the result is:", sum)
+
+# print(type("234"))
+
+
+
+# sum = "343" + "afdasfs43425" + "0"
+# print(sum)
+
